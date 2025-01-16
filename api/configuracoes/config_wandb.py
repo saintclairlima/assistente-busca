ddb94c800f499dd15b8c913ddf5af592b5ab24d0
Saint-Clair <caehevanras@gmail.com>
2025-01-16 12:22:19 -0300

    Ajusta nome de variável (prefixo mudado para equipe)

--- api/configuracoes/config_wandb.py
+++ api/configuracoes/config_wandb.py
@@ -4,8 +4,8 @@ class ConfiguracoesWeightsAndBiases:
     def __init__(self):
         
         self.usar_wandb=True
-        self.wandb_prefixo='cleiane-projetos' # grumpyai
+        self.wandb_equipe='cleiane-projetos' # grumpyai
         self.wandb_nome_projeto='assistente-busca'
         self.wandb_tipo_execucao='producao'
-        self.wandb_uri_artefato_banco_vetorial=f'{self.wandb_prefixo}/{self.wandb_nome_projeto}/banco-vetorial-chroma:latest',
+        self.wandb_uri_artefato_banco_vetorial=f'{self.wandb_equipe}/{self.wandb_nome_projeto}/banco-vetorial-chroma:latest',
         # AFAZER: colocar artefato do prompt
