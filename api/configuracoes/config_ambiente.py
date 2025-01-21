f4ede807b1d5b33f6cb9dff3b9e0d5a4fd566562
Saint-Clair <caehevanras@gmail.com>
2025-01-21 08:33:37 -0300

    Altera .env.TEMPLATE, incluindo variáveis de banco de dados

--- api/configuracoes/config_ambiente.py
+++ api/configuracoes/config_ambiente.py
@@ -12,6 +12,10 @@ class ConfiguracoesAmbiente:
         self.url_llm=os.getenv('URL_LLM')
         self.url_host=os.getenv('URL_HOST')
         self.url_banco_sql=os.getenv('URL_BANCO_SQL')
+        self.porta_banco_sql=os.getenv('PORTA_BANCO_SQL')
+        self.usuario_banco_sql=os.getenv('USER_BANCO_SQL')
+        self.senha_banco_sql=os.getenv('SENHA_BANCO_SQL')
+        self.database_banco_sql=os.getenv('DATABASE_BANCO_SQL')
         
         self.device=os.getenv('DEVICE') # ['cpu', cuda']
         self.ambiente_execucao=os.getenv('AMBIENTE_EXECUCAO')
