0cbf443ccd3aa7097c8e9d32e1ce35a8ca7bd996
Saint-Clair <caehevanras@gmail.com>
2025-01-23 10:03:11 -0300

    Atualiza arquivo de setup para windows

--- setup.bat
+++ setup.bat
@@ -1,18 +1,22 @@
 echo "Preparando os arquivos necessários..."
 cp api\.env.TEMPLATE api\.env
-tar -xf .\bancos_vetores.zip
+tar -xf .\api\dados\bancos_vetores\bancos_vetores.zip -C .\api\dados\bancos_vetores
 rmdir /s /q api\dados\bancos_vetores\banco_vetores_alrn
 rmdir /s /q api\dados\bancos_vetores\banco_vetores_regimento_resolucoes_rh
 rmdir /s /q api\dados\bancos_vetores\resultados_testes
+echo "\n\n\n"
 
 echo "Criando ambiente virtual python..."
 python -m venv chat-env
-source chat-env\Script\activate
+chat-env\Scripts\activate
+echo "\n\n\n"
 
 echo "Instalando dependências..."
 pip install -r requirements.txt
+echo "\n\n\n"
 
 echo "Criando banco SQLite para persistência das interações..."
 python -m api.dados.persistencia
+echo "\n\n\n"
 
 echo "Concluído!"
