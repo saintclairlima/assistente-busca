8e6d0bea505e37540b9b2e7a4adedbc5969d155e
Saint-Clair <caehevanras@gmail.com>
2025-01-23 09:30:53 -0300

    Ajsuta mensagens do setup

--- setup.sh
+++ setup.sh
@@ -4,15 +4,19 @@ unzip api/dados/bancos_vetores/bancos_vetores.zip -d api/dados/bancos_vetores
 rm -R api/dados/bancos_vetores/banco_vetores_alrn
 rm -R api/dados/bancos_vetores/banco_vetores_regimento_resolucoes_rh
 rm -R api/dados/bancos_vetores/resultados_testes
+echo "\n\n\n"
 
 echo "Criando ambiente virtual python..."
 python3 -m venv chat-env
 source chat-env/bin/activate
+echo "\n\n\n"
 
 echo "Instalando dependências..."
 pip install -r requirements.txt
+echo "\n\n\n"
 
 echo "Criando banco SQLite para persistência das interações..."
 python3 -m api.dados.persistencia
+echo "\n\n\n"
 
 echo "Concluído!"
