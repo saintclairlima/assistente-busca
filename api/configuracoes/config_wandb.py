e290838e2450c58ef50096c0a4d33a5bf2545e3e
Saint-Clair Lima <caehevanras@gmail.com>
2025-01-30 12:25:24 -0300

    Inclui campos de chave de APIs de serviços externos

--- api/configuracoes/config_wandb.py
+++ api/configuracoes/config_wandb.py
@@ -1,3 +1,5 @@
+import os
+import json
 
 
 class ConfiguracoesWeightsAndBiases:
@@ -6,9 +8,12 @@ class ConfiguracoesWeightsAndBiases:
         with open('api/configuracoes/arq_conf.json', 'r', encoding='utf-8') as arq:
             configs = json.load(arq)
         
-        self.usar_wandb=configs['usar_wandb']
-        self.wandb_equipe=configs['wandb_equipe']
-        self.wandb_nome_projeto=configs['wandb_nome_projeto']
-        self.wandb_tipo_execucao=configs['wandb_tipo_execucao']
-        self.wandb_uri_artefato_banco_vetorial=f'{self.wandb_equipe}/{self.wandb_nome_projeto}/banco-vetorial-chroma:latest'
-        self.wandb_uri_artefato_prompt_mensagens=f'{self.wandb_equipe}/{self.wandb_nome_projeto}/prompts-mensagens:latest'
+        self.usar_wandb = configs['usar_wandb']
+        self.wandb_equipe = configs['wandb_equipe']
+        self.wandb_nome_projeto = configs['wandb_nome_projeto']
+        self.wandb_tipo_execucao = configs['wandb_tipo_execucao']
+        self.wandb_uri_artefato_banco_vetorial = f'{self.wandb_equipe}/{self.wandb_nome_projeto}/banco-vetorial-chroma:latest'
+        self.wandb_uri_artefato_prompt_mensagens = f'{self.wandb_equipe}/{self.wandb_nome_projeto}/prompts-mensagens:latest'
+
+        os.environ['OPENAI_API_KEY'] = configs['wandb_api_key']
+        os.environ['WANDB_API_KEY'] = configs['openai_api_key']
